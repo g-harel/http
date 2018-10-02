3b2929fb5fe0c250872e9e1ed2826675ad8a6b42
Gabriel <gabrielj.harel@gmail.com>
2018-10-01 23:56:28 -0400

    split http function into logical chunks

--- httpc.go
+++ httpc.go
@@ -14,29 +14,36 @@ import (
 type Request struct {
 	Method  string
 	URL     string
+	url     *url.URL
 	Headers *Headers
 	Body    io.Reader
 }
 
 // Response represents a received HTTP response.
 type Response struct {
+	Version    string
+	Status     string
 	StatusCode int
 	Headers    *Headers
 	Body       io.Reader
 }
 
-// HTTP executes an HTTP request.
-func HTTP(req *Request, log io.Writer) (*Response, error) {
-	// Input url is parsed and validated.
+// Validate checks that the input request is valid.
+// If the function does not error, the input request's `url` field will have been assigned a value.
+func validate(req *Request) error {
+	if req.Method == "GET" && req.Body != nil {
+		return fmt.Errorf("Cannot write body to GET request")
+	}
+
 	u, err := url.Parse(req.URL)
 	if err != nil {
-		return nil, fmt.Errorf("could not parse given url: %v", err)
+		return fmt.Errorf("could not parse given url: %v", err)
 	}
 	if u.Scheme == "" {
-		return nil, fmt.Errorf("missing protocol in \"%v\"", u.String())
+		return fmt.Errorf("missing protocol in \"%v\"", u.String())
 	}
 	if u.Scheme != "http" {
-		return nil, fmt.Errorf("unknown protocol \"%v\" in \"%v\"", u.Scheme, u.String())
+		return fmt.Errorf("unknown protocol \"%v\" in \"%v\"", u.Scheme, u.String())
 	}
 	if u.Port() == "" {
 		u.Host += ":80"
@@ -45,105 +52,141 @@ func HTTP(req *Request, log io.Writer) (*Response, error) {
 		u.Path = "/"
 	}
 
-	// Headers are copied from the given reference.
-	h := &Headers{}
-	h.AddCopy(req.Headers)
+	// Validated URL is added to the request's private field.
+	req.url = u
 
+	return nil
+}
+
+// Request writes request data to the given connection.
+func request(conn io.Writer, req *Request) error {
 	// Host header is written with the value extracted from the url.
-	h.Add("Host", u.Hostname())
-
-	// Open TCP connection to host.
-	conn, err := net.Dial("tcp", u.Host)
-	if err != nil {
-		return nil, fmt.Errorf("could connect to host: %v", err)
-	}
-	defer conn.Close()
-
-	// All data written to the connection is mirrored into the log writer.
-	w := io.MultiWriter(conn, log)
+	req.Headers.Add("Host", req.url.Hostname())
 
 	// Write request status line.
-	_, err = fmt.Fprintf(w, "%v %v HTTP/1.0\r\n", req.Method, u.RequestURI())
+	_, err := fmt.Fprintf(conn, "%v %v HTTP/1.0\r\n", req.Method, req.url.RequestURI())
 	if err != nil {
-		return nil, fmt.Errorf("could not write request line: %v", err)
+		return fmt.Errorf("could not write request line: %v", err)
 	}
 
 	// Write header lines.
-	err = h.Fprint(w)
+	err = req.Headers.Fprint(conn)
 	if err != nil {
-		return nil, fmt.Errorf("could not write headers: %v", err)
+		return fmt.Errorf("could not write headers: %v", err)
 	}
 
 	// Write empty line to signal end of headers.
-	_, err = fmt.Fprintf(w, "\r\n")
+	_, err = fmt.Fprintf(conn, "\r\n")
 	if err != nil {
-		return nil, fmt.Errorf("could not write to request: %v", err)
+		return fmt.Errorf("could not write: %v", err)
 	}
 
 	// Write request body.
 	if req.Body != nil {
-		_, err := io.Copy(w, req.Body)
+		_, err := io.Copy(conn, req.Body)
 		if err != nil {
-			return nil, fmt.Errorf("could not write data to request: %v", err)
+			return fmt.Errorf("could not write data: %v", err)
 		}
-
-		// Formatting errors are not critical.
-		_, _ = fmt.Fprintf(log, "\n")
 	}
 
+	return nil
+}
+
+// Response reads response data from a given connection.
+func response(conn io.Reader) (*Response, error) {
 	res := &Response{
 		Headers: &Headers{},
 	}
 
 	// Reader is used to read response line by line.
 	reader := bufio.NewReader(conn)
-	isEOF := false
 
 	// Response body is read from the remaining data in the reader.
 	res.Body = reader
 
 	// Read first line of response (status line).
 	line, err := reader.ReadString('\n')
-	if err == io.EOF {
-		isEOF = true
-	} else if err != nil {
-		return nil, fmt.Errorf("could not read response status line: %v", err)
-	}
-	_, err = fmt.Fprintf(log, line)
-	if err != nil {
-		return nil, fmt.Errorf("could not copy response line to output log: %v", err)
+	if err != nil && err != io.EOF {
+		return nil, fmt.Errorf("could not read status line: %v", err)
 	}
 	sl := strings.Split(line, " ")
-	if len(sl) < 2 {
-		return nil, fmt.Errorf("could not parse response status line: %v", line)
+	if len(sl) < 3 {
+		return nil, fmt.Errorf("could not parse status line: %v", line)
 	}
+	res.Version = sl[0]
 	res.StatusCode, err = strconv.Atoi(sl[1])
 	if err != nil {
-		return nil, fmt.Errorf("could not parse response status code: %v", err)
+		return nil, fmt.Errorf("could not parse status code: %v", err)
 	}
-
-	// If the first read has consumed the entire response, there is no need to proceed.
-	if isEOF {
-		return res, nil
-	}
+	res.Status = sl[2]
 
 	// Read and parse header lines.
 	for {
 		line, err := reader.ReadString('\n')
 		if err == io.EOF {
-			return res, nil
-		} else if err != nil {
-			return nil, fmt.Errorf("could not read header line: %v", err)
+			break
 		}
-		_, err = fmt.Fprintf(log, line)
 		if err != nil {
-			return nil, fmt.Errorf("could not copy response line to output log: %v", err)
+			return nil, fmt.Errorf("could not read header line: %v", err)
 		}
 		line = strings.TrimSpace(line)
 		if line == "" {
 			break
 		}
 		res.Headers.AddRaw(line)
+	}
+
+	return res, nil
+}
+
+// HTTP executes an HTTP request.
+func HTTP(req *Request, log io.Writer) (*Response, error) {
+	// Validate request.
+	err := validate(req)
+	if err != nil {
+		return nil, fmt.Errorf("could validate request: %v", err)
+	}
+
+	// Open TCP connection to host.
+	conn, err := net.Dial("tcp", req.url.Host)
+	if err != nil {
+		return nil, fmt.Errorf("could connect to host: %v", err)
+	}
+	defer conn.Close()
+
+	// All data written to the connection is mirrored into the log.
+	w := io.MultiWriter(conn, log)
+
+	// Write request to connection.
+	err = request(w, req)
+	if err != nil {
+		return nil, fmt.Errorf("could not write request: %v", err)
+	}
+
+	// Read and parse response from connection.
+	res, err := response(conn)
+	if err != nil {
+		return nil, fmt.Errorf("could not read response: %v", err)
+	}
+
+	// Response status line and headers are written to the log.
+	if req.Body != nil {
+		_, err = fmt.Fprintf(log, "\n")
+		if err != nil {
+			return nil, fmt.Errorf("could write to log: %v", err)
+		}
+	}
+	_, err = fmt.Fprintf(log, "%v %v %v", res.Version, res.StatusCode, res.Status)
+	if err != nil {
+		return nil, fmt.Errorf("could not log response status line: %v", err)
+	}
+	err = res.Headers.Fprint(log)
+	if err != nil {
+		return nil, fmt.Errorf("could not log response headers: %v", err)
+	}
+	_, err = fmt.Fprintf(log, "\n")
+	if err != nil {
+		return nil, fmt.Errorf("could write to log: %v", err)
 	}
 
 	return res, nil
